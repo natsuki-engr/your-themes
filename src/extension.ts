0670145d5656a2aa6cb1c73ad997ac98acca8822
natsuki-engr <biafrajr@gmail.com>
2024-06-08 14:06:22 +0900

    add sample webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,61 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "theme-view" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "theme-view" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  // let disposable = vscode.commands.registerCommand('theme-view.helloWorld', () => {
+  // 	// The code you place here will be executed every time your command is executed
+  // 	// Display a message box to the user
+  // 	vscode.window.showInformationMessage('Hello World from theme-view!');
+  // });
+  let disposable = vscode.commands.registerCommand(
+    "theme-view.viewThemes",
+    (editBuilder, _editor, _selection, dex) => {
+      const panel = vscode.window.createWebviewPanel(
+        "viewThemes",
+        "view your local themes",
+        vscode.ViewColumn.Active
+      );
+      const scriptPath = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "media", "script.js")
+      );
+      const cssStyle = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "./view/style", "view.css")
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('theme-view.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from theme-view!');
-	});
+      panel.webview.html = `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link rel="stylesheet" type="text/css" href="${cssStyle}" />
+				<script src="${scriptPath}"></script>
+			</head>
+			<body>
+			<div class="container">
+				<div class="form">
+					<code>Title</code>
+					<input />
+					<code>Code</code>
+					<textarea></textarea>
+					<button>Submit</button>
+				</div>
+			</div>
+			</body>
+			</html>`;
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated

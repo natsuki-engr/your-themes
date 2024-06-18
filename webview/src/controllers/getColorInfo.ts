7d5d6fcdcd9bf51cd90a0782644a6190845a2a4f
natsuki ueda <63272932+natsuki-engr@users.noreply.github.com>
2024-06-18 03:32:24 +0000

    remove debug code

--- webview/src/controllers/getColorInfo.ts
+++ webview/src/controllers/getColorInfo.ts
@@ -6,7 +6,6 @@ import { SvgColors, isSvgColors } from "../../../src/types/svgColors";
 export const getColorInfo = async (messageListener: MessageListener, themeDir: string, themePathListByLabel: Record<ThemeInfo['label'], ThemeInfo['path']>): Promise<Record<string, SvgColors>> => {
   return new Promise<Record<string, SvgColors>>(resolve => {
     messageListener.receive("resp-of-get-group-color-themes", (json) => {
-      isResponse(json) ? console.log('themeDir === json.groupDir', themeDir === json.groupDir) : '';
       if(isResponse(json) && themeDir === json.groupDir) {
         resolve(json.themes)
         return true

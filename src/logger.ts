356cf4270c2bad86d7db7d92d19a14b60d1bb34c
natsuki ueda <biafrajr@gmail.com>
2025-01-25 09:48:40 +0900

    enhance logger to support logging objects as well as strings

--- src/logger.ts
+++ src/logger.ts
@@ -3,14 +3,19 @@ import * as vscode from "vscode";
 let channel: vscode.OutputChannel | null = null;
 
 export default class Logger {
-  static log(msg: string) {
+  static log(msg: string | object) {
     const date = new Date();
     // prettier-ignore
     const timestamp = date.getFullYear() + '-' + pad2(date.getMonth() + 1) + '-' + pad2(date.getDate()) + ' ' + pad2(date.getHours()) + ':' + pad2(date.getMinutes()) + ':' + pad2(date.getSeconds()) + '.' + pad3(date.getMilliseconds());
     if (channel === null) {
       channel = vscode.window.createOutputChannel("Your Themes");
     }
-    channel.appendLine("[" + timestamp + "] " + msg);
+
+    if (typeof msg === "string") {
+      channel.appendLine("[" + timestamp + "] " + msg);
+    } else {
+      channel.appendLine("[" + timestamp + "] " + JSON.stringify(msg));
+    }
   }
 }
 

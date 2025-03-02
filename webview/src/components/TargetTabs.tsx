dcd99119bb90cfb21424c64ef4cd18de0eccfe0f
natsuki ueda <biafrajr@gmail.com>
2025-03-02 23:55:05 +0900

    feat: refactor TargetTabs component to use dynamic target list and improve label rendering

--- webview/src/components/TargetTabs.tsx
+++ webview/src/components/TargetTabs.tsx
@@ -8,6 +8,12 @@ import { getConfigTargets } from "../controllers/getConfigTargets";
 import MessageListener from "../controllers/messageListener";
 import { useStaticSWR } from "../stores/useStaticSWR";
 
+const toTitleCase = (s: string) => {
+  const arr = s.split("");
+  const head = (arr as string[]).shift() as string;
+  return head.toUpperCase() + arr.join("");
+};
+
 const TargetTabs: React.FC = () => {
   const { data: target, mutate: setTarget } =
     useStaticSWR<ConfigTargetValueType>("config-target", ConfigTarget.User);
@@ -31,38 +37,33 @@ const TargetTabs: React.FC = () => {
     mutate((key) => key === "get-current-theme-label", undefined, true);
   };
 
+  const targetList = [
+    ConfigTarget.User,
+    workspaceFolders.length ? ConfigTarget.Workspace : undefined,
+  ]
+    .filter((v) => v !== undefined)
+    .map((t) => ({
+      value: t,
+      label: toTitleCase(t),
+    }));
+
   return (
     <ul className="bg-vscode-editor-background sideBarSectionHeader-border sticky top-0 z-10 flex h-12 w-full items-end pe-2 ps-2 pt-2">
-      <li className="block">
-        <label className="block cursor-pointer border-2 border-solid border-transparent p-3 has-[:checked]:border-b-[var(--vscode-activityBar-activeBorder)]">
-          User
-          <input
-            onChange={() => changeHandler(ConfigTarget.User)}
-            className="pointer-events-none absolute opacity-0"
-            type="radio"
-            value={ConfigTarget.User}
-            checked={target === ConfigTarget.User}
-            name="config_target"
-          />
-        </label>
-      </li>
-      {workspaceFolders.length ? (
+      {targetList.map((t) => (
         <li className="block">
           <label className="block cursor-pointer border-2 border-solid border-transparent p-3 has-[:checked]:border-b-[var(--vscode-activityBar-activeBorder)]">
-            Workspace
+            {t.label}
             <input
-              onChange={() => changeHandler(ConfigTarget.Workspace)}
+              onChange={() => changeHandler(t.value)}
               className="pointer-events-none absolute opacity-0"
               type="radio"
-              value={ConfigTarget.Workspace}
-              checked={target === ConfigTarget.Workspace}
+              value={t.value}
+              checked={target === t.value}
               name="config_target"
             />
           </label>
         </li>
-      ) : (
-        <></>
-      )}
+      ))}
     </ul>
   );
 };

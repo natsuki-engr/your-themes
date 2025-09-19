6df90210c699f82b7163511efa58c55ba7e26661
natsuki ueda <63272932+natsuki-engr@users.noreply.github.com>
2025-09-19 08:48:29 +0000

    test: add VRT test with vitest browser mode(beta feature) for `VscodeSvg.tsx`
    Changes:
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: N/A

--- webview/vitest.browser.config.ts
+++ webview/vitest.browser.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser/providers/playwright";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -6,7 +7,7 @@ export default defineConfig({
   test: {
     browser: {
       enabled: true,
-      provider: "playwright",
+      provider: playwright(),
       // https://vitest.dev/guide/browser/playwright
       instances: [{ browser: "chromium", headless: true }],
     },
